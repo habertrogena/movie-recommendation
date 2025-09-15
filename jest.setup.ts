80c3544da49c5a328094478a218bd8e2fa72b76b
habertrogena <habertorogy12@gmail.com>
2025-09-15 11:14:59 +0300

    creates test for components and hooks

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom";
+import "whatwg-fetch";
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve({}),
+  })
+) as jest.Mock;
