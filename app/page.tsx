0e294f38d87e5886ad0e51f88604ef676f953cc4
habertrogena <habertorogy12@gmail.com>
2025-09-15 11:14:19 +0300

    refactor homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import { useState } from "react";
 import { usePopularMovies } from "@/hooks/usePopularMovies";
 import { useSearchMovies } from "@/hooks/useSearchMovies";
 import LoadingMovies from "@/components/LoadingMovies";
 import ErrorMessage from "@/components/ErrorMessage";
-import MovieCard from "@/components/MovieCard";
-import PaginationControls from "@/components/PaginationControls";
-import { useState } from "react";
+import TopBar from "@/components/layout/TopBar";
+import MoviesGrid from "@/components/movies/MoviesGrid";
+import MoviesPagination from "@/components/movies/MoviesPagination";
 
 export default function HomePage() {
   const [page, setPage] = useState(1);
@@ -22,44 +23,32 @@ export default function HomePage() {
   if (isError) return <ErrorMessage message="Failed to fetch movies." />;
 
   return (
-    <div className="p-4 sm:p-6">
-      <div className="mb-6 flex justify-center px-2">
-        <div className="relative w-full sm:w-3/4 md:w-1/2">
-          <input
-            type="text"
-            value={query}
-            onChange={(e) => {
-              setQuery(e.target.value);
-              setPage(1);
-            }}
-            placeholder="Search movies by title or keyword..."
-            className="w-full pl-10 pr-4 py-3 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm sm:text-base"
-          />
-          <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
-            🔍
-          </span>
-        </div>
-      </div>
+    <div className="min-h-screen bg-slate-200">
+      <div className="max-w-7xl mx-auto p-4 sm:p-6">
+        <TopBar
+          query={query}
+          setQuery={(q) => {
+            setQuery(q);
+            setPage(1);
+          }}
+        />
 
-      <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6 p-2 sm:p-4 md:p-6">
-        {data?.results.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </main>
+        {data && <MoviesGrid movies={data.results} />}
 
-      {data && (
-        <div className="mt-6 flex justify-center px-2">
-          <PaginationControls
+        {data && (
+          <MoviesPagination
             page={data.page}
             totalPages={data.total_pages}
             onPageChange={setPage}
           />
-        </div>
-      )}
+        )}
 
-      {isFetching && (
-        <p className="text-center text-xs sm:text-sm mt-2">Loading page...</p>
-      )}
+        {isFetching && (
+          <p className="text-center text-xs sm:text-sm mt-2 text-gray-500">
+            Loading page...
+          </p>
+        )}
+      </div>
     </div>
   );
 }
